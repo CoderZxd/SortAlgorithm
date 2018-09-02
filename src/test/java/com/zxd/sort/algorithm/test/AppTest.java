714cae4bb741c96cea371e58237cc8bda7f60ff4
Admin <zxd_0825@163.com>
2018-09-03 00:40:21 +0800

    init project

---
+++ src/test/java/com/zxd/sort/algorithm/test/AppTest.java
@@ -0,0 +1,20 @@
+package com.zxd.sort.algorithm.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
