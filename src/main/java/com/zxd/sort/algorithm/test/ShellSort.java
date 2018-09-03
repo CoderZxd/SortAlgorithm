b92a07529d5bcfef48211fd3ea917ea3458b9f04
Admin <zxd_0825@163.com>
2018-09-04 00:01:19 +0800

    shell sort

---
+++ src/main/java/com/zxd/sort/algorithm/test/ShellSort.java
@@ -0,0 +1,22 @@
+package com.zxd.sort.algorithm.test;
+
+/**
+ * @author CoderZZ
+ * @Title: ${FILE_NAME}
+ * @Project: SortAlgorithm
+ * @Package com.zxd.sort.algorithm.test
+ * @description: 1959年Shell发明，第一个突破O(n2)的排序算法，是简单插入排序的改进版。它与插入排序的不同之处在于，
+ * 它会优先比较距离较远的元素。希尔排序又叫缩小增量排序。
+ *
+ * 不稳定算法。
+ *
+ * 在要排序的一组数中，根据某一增量分为若干子序列，并对子序列分别进行插入排序。
+ * 然后逐渐将增量减小,并重复上述过程。直至增量为1,此时数据序列基本有序,最后进行插入排序。
+ * @Version 1.0
+ * @create 2018-09-03 23:51
+ **/
+public class ShellSort {
+    public static void main(String[] args){
+        
+    }
+}
